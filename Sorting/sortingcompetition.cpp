642b5097546299a3ebb7bafd5e1948bddee18a74
Quincy Schurr <qschurr@smu.edu>
2014-10-20 20:09:34 -0500

    updated documentation

--- Sorting/sortingcompetition.cpp
+++ Sorting/sortingcompetition.cpp
@@ -154,6 +154,7 @@ void SortingCompetition::lengthAlpha2(char**& wordsLength)
             {
                 break;
             }
+
             else //iterates counters
             {
                 lengthCount++;
